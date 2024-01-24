1865e94c3820c827798b1d1282fab319c8e2acfa
monks <a@monks.co>
2024-01-24 00:46:52 -0600

    add local/sessionstorage library

---
+++ storage/storage.go
@@ -0,0 +1,24 @@
+//go:build js || wasm
+
+package storage
+
+import "syscall/js"
+
+type Storage string
+
+const (
+	Local   Storage = "localStorage"
+	Session Storage = "sessionStorage"
+)
+
+func (s Storage) Get(k string) string {
+	got := js.Global().Get(string(s)).Call("getItem", k)
+	if !got.Truthy() {
+		return ""
+	}
+	return got.String()
+}
+
+func (s Storage) Set(k, v string) {
+	js.Global().Get(string(s)).Call("setItem", k, v)
+}
